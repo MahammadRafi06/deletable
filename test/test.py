97d07ca01a5b0bb4212123f0d9e3ccd0039c2dd1
MahammadRafi06 <mrafi@sjmsom.in>
2025-05-05 22:09:36 -0700

    test completed

--- test/test.py
+++ test/test.py
@@ -1,9 +1,4 @@
-import sys
-import os
-
-# Add the parent directory of 'utils' to sys.path
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from app import app
+from src.app import app
 
 def test_home():
     response = app.test_client().get("/")
